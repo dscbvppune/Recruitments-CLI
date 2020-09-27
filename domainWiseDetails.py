e8a3c08a64609adb7c402fcde791b457ea1eaa08
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-09-27 20:38:16 +0530

    Update domainWiseDetails.py

--- domainWiseDetails.py
+++ domainWiseDetails.py
@@ -4,6 +4,9 @@ def technicalDomainDetails(f):
 	print(f.renderText("Technical Team"))
 	print("\n\n\n\n")
 	print("Peeps interested in building => Web apps, Mobile apps, ML, Cloud, Ethical Hacking, AR / VR, Miscellaneous")
+	details = """The technical team consists of people who like to work in the domain of Web Development, Mobile App Development, Machine Learning and Google Cloud. We welcome students who are keen on learning new things. Even if you haven’t started your tech journey yet, we would insist that you join us as a technical team member. We would provide adequate resources to get you started on your journey."""
+	print("\n\n")
+	print(details
 
 def designingDomainDetails(f):
 	print(f.renderText("DSC BVP Pune"))
@@ -11,17 +14,26 @@ def designingDomainDetails(f):
 	print(f.renderText("Designing Team"))
 	print("\n\n\n\n")
 	print("Peeps who are => Designers, UI / UX designers, Video Editors")
+	details = """The designing team consists of people who like to work in the domain of graphic designing, UI/UX, Video editing and 3D modelling and rendering. We aren't only looking for professionals, if you are interested in designing but haven't started yet, whether you are just a beginner, or a professional with excellent skills, this is your place to learn, practice and grow."""
+	print("\n\n")
+	print(details
 
 def managementDomainDetails(f):
-	print(f.renderText("DSC BVP Pune"))
-	print("\n\n")
-	print(f.renderText("Content Team"))
-	print("\n\n\n\n")
-	print("For content writers")
-
-def contentDomainDetails(f):
 	print(f.renderText("DSC BVP Pune"))
 	print("\n\n")
 	print(f.renderText("Management Team"))
 	print("\n\n\n\n")
 	print("For peeps who can come up with smart ideas for event management and suggest good publicity strategies")
+	details = """The management team consists of strategy makers, event organizer, digital marketers, anchor and a publicist. We would good to have those students who can make best out of every situation. We will provide a good platform to improve your management and technical skills as a DSC management team member."""
+	print("\n\n")
+	print(details)
+
+def contentDomainDetails(f):
+	print(f.renderText("DSC BVP Pune"))
+	print("\n\n")
+	print(f.renderText("Content Team"))
+	print("\n\n\n\n")
+	print("For content writers")
+	details = """The content team is tasked with creating reports for summarising various events, documentations for projects, script-writing for various videos as well as coming up with quirky and attractive captions for our social media handles. If you think you can contribute something tangible in the course of a year, or if you just want to feel the currents of content writing in general, feel free to hop on!"""
+	print("\n\n")
+	print(details)
